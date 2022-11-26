8760a85a646543247680122f1bbf162788ed4e39
Shell M. Shrader <shell@shellware.com>
2022-11-26 18:59:27 -0500

    a few fixes

--- stats.py
+++ stats.py
@@ -13,6 +13,8 @@ import subprocess
 import busio
 import adafruit_ssd1306
 
+import requests
+
 from PIL import Image, ImageDraw, ImageFont
 from board import SCL, SDA
 
@@ -40,22 +42,25 @@ splashImage = Image.new("1", (width, height))
 splashDraw = ImageDraw.Draw(splashImage)
 
 # Draw a black filled box to clear the image.
-splashDraw.rectangle((0, 0, width, height), outline=0, fill=0)
+# splashDraw.rectangle((0, 0, width, height), outline=0, fill=0)
 
 # Load fonts
 # font = ImageFont.load_default()
 headerFont = ImageFont.truetype('/home/pi/lcdstats/source-code-pro/SourceCodePro-Regular.ttf', 8)
-# headerFont = ImageFont.truetype('/home/pi/lcdstats/misc-fixed.ttf', 9)
-valueFont = ImageFont.truetype('/home/pi/lcdstats/source-code-pro/SourceCodePro-Regular.ttf', 8)
+valueFont = ImageFont.truetype('/home/pi/lcdstats/source-code-pro/SourceCodePro-Regular.ttf', 9)
 splashFont = ImageFont.truetype('/home/pi/lcdstats/source-code-pro/SourceCodePro-Regular.ttf', 12)
+
+# headerFont = ImageFont.truetype('/home/pi/lcdstats/misc-fixed.ttf', 9)
+# valueFont = ImageFont.truetype('/home/pi/lcdstats/misc-fixed.ttf', 10)
+# splashFont = ImageFont.truetype('/home/pi/lcdstats/misc-fixed.ttf', 12)
 
 s1 = "webcamd-lcd"
 s2 = "------------"
 s3 = "By Shellware"
 
 s1w, s1h = splashFont.getsize(s1)
-s2w, s2h = headerFont.getsize(s2)
-s3w, s3h = headerFont.getsize(s3)
+s2w, s2h = valueFont.getsize(s2)
+s3w, s3h = valueFont.getsize(s3)
 
 splashDraw.text((width / 2 - s1w / 2, 0),  s1, font=splashFont, fill=255)
 splashDraw.text((width / 2 - s2w / 2, s1h + 1),  s2, font=valueFont, fill=255)
@@ -66,8 +71,8 @@ disp.show()
 
 time.sleep(10)
 
-# cmd = "nproc"
-# cpus = int(subprocess.check_output(cmd, shell=True).decode("utf-8").replace("\n", ""))
+cmd = "nproc"
+cpus = int(subprocess.check_output(cmd, shell=True).decode("utf-8").replace("\n", ""))
 
 image = Image.new("1", (height, width))
 final = Image.new("1", (width, height))
@@ -79,44 +84,60 @@ cmd = "hostname"
 hostname = subprocess.check_output(cmd, shell=True).decode("utf-8").replace("\n", "")
 sessions = -1
 
+url = 'http://localhost:8080/?info'
+
 while True:
-    cmd = "curl 'http://localhost:8080/?info' -s | jq -r '.config.port, .stats.encodeFps, .stats.sessionCount, .stats.avgStreamFps'"
-    webcam = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    stats = webcam.replace("\r", "").split("\n")
+    # cmd = "curl 'http://localhost:8080/?info' -s | jq -r '.config.port, .stats.encodeFps, .stats.sessionCount, .stats.avgStreamFps'"
+    # webcam = subprocess.check_output(cmd, shell=True).decode("utf-8")
+    # stats = webcam.replace("\r", "").split("\n")
 
     try:
-        port = int(stats[0])
-        encodeFps = float(stats[1])
-        streamFps = float(stats[3])
-
-        # dim the screen if we don't have any active cients
-        if sessions != 0 and int(stats[2]) == 0:
-            sessions = 0
-            splashDraw.rectangle((0, 0, width, height), outline=0, fill=0)
-            disp.image(splashImage)
-            disp.show()
+        resp = requests.get(url=url)
+        if resp.status_code != 200:
             time.sleep(5)
+            print("%s: unable to query webcamd: response code = [%d]" % (datetime.datetime.now(), resp.status_code), flush=True)
             continue
 
-        sessions = int(stats[2])
-        if sessions == 0: continue
+        data = resp.json()
+
+        encodeFps = data["stats"]["encodeFps"]
+        streamFps = data["stats"]["avgStreamFps"]
+        port = data["config"]["port"]
+
+        # dim the screen if we don't have any active cients
+        if sessions != 0 and data["stats"]["sessionCount"] == 0:
+            sessions = 0
+            disp.fill(0)
+            disp.show()
+            time.sleep(10)
+            continue
+
+        sessions = data["stats"]["sessionCount"]
+
+        if sessions == 0:
+            time.sleep(10)
+            continue
+
+
     except Exception as e:
         print("%s: unable to query webcamd: [%s]" % (datetime.datetime.now(), e), flush=True)
         sessions = 0
-        splashDraw.rectangle((0, 0, width, height), outline=0, fill=0)
-        disp.image(splashImage)
+        disp.fill(0)
         disp.show()
         time.sleep(5)
         continue
 
-    cmd = "mpstat 5 1 -o JSON | jq -r '.sysstat.hosts[0].statistics[0].\"cpu-load\"[0].idle'"
-    cpu = 100. - float(subprocess.check_output(cmd, shell=True).decode("utf-8"))
+    # cmd = "mpstat 5 1 -o JSON | jq -r '.sysstat.hosts[0].statistics[0].\"cpu-load\"[0].idle'"
+    # cpu = 100. - float(subprocess.check_output(cmd, shell=True).decode("utf-8"))
 
     # Shell scripts for system monitoring from here:
     # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    # cmd = 'cut -f 1 -d " " /proc/loadavg'
-    # load = float(subprocess.check_output(cmd, shell=True).decode("utf-8"))
-    # cpu = load / cpus * 100.
+
+    cmd = 'cut -f 1 -d " " /proc/loadavg'
+    load = float(subprocess.check_output(cmd, shell=True).decode("utf-8"))
+    cpu = load / cpus * 100.
+    if cpu > 100: cpu = 100
+
     # cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
     # MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
     # cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
@@ -146,7 +167,7 @@ while True:
     cH = "ENCODE"
     cHw, cHh = headerFont.getsize(cH)
 
-    top = top + cVh + 8
+    top = top + cVh + 6
     draw.text((height / 2 - cHw / 2, top), cH, font=headerFont, fill=255, spacing=4)
 
     top = top + cHh + 1
@@ -163,7 +184,7 @@ while True:
     cH = "STREAM"
     cHw, cHh = headerFont.getsize(cH)
 
-    top = top + cVh + 8
+    top = top + cVh + 6
     draw.text((height / 2 - cHw / 2, top), cH, font=headerFont, fill=255, spacing=4)
 
     top = top + cHh + 1
@@ -181,7 +202,7 @@ while True:
     cH = "ACTIVE"
     cHw, cHh = headerFont.getsize(cH)
 
-    top = top + cVh + 8
+    top = top + cVh + 6
     draw.text((height / 2 - cHw / 2, top), cH, font=headerFont, fill=255, spacing=4)
 
     cV = "%d" % sessions
@@ -198,12 +219,14 @@ while True:
     finalDraw.rectangle((0, 0, width, height), outline=0, fill=0)
     final.paste(rot,(0, 0, sx, sy), rot)
 
+    # Display image.
+    disp.image(final)
+    disp.show()
+
     # print("---------------------------------------")
     # print("http://%s:%d" % (hostname, port), flush=False)
     # print("CPU @ %.2f%% busy" % cpu, flush=False)
     # print("Encoding @ %.1ffps" % encodeFps, flush=False)
     # print("%d sessions @ avg %.1ffps" % (sessions, streamFps), flush=True)
 
-    # Display image.
-    disp.image(final)
-    disp.show()
+    time.sleep(5)
